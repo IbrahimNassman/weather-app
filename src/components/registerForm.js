22a602cf5fb1cb25584ad3fe1ded84b984488fef
unknown <emanelzayadi0@gmail.com>
2022-04-06 12:33:29 +0200

    create registerForm and navbar components

--- src/components/registerForm.js
+++ src/components/registerForm.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 
 
 const RegisterForm = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(JSON.stringify(data));
 
   return (
-    <div>Register Page</div>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("firstName", { required: true })} placeholder="First name" />
+
+      <input {...register("lastName", { minLength: 2 })} placeholder="Last name" />
+      <input  type="password"  {...register("password", { pattern: /^[A-Za-z]+$/i })} />
+      <select {...register("gender")}>
+        <option value="female">female</option>
+        <option value="male">male</option>
+        <option value="other">other</option>
+      </select>
+      
+
+      <input type="submit" />
+    </form>
+    </div>
     
   )
 }
